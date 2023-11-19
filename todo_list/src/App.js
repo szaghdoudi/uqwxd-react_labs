d8707b2f63828b74060e97ab11f005f8e58aed42
s.zaghdoudi <sofiene.zaghdoudi@gmail.com>
2023-11-19 17:09:11 +0100

    [F1]Add todo task

--- todo_list/src/App.js
+++ todo_list/src/App.js
@@ -3,7 +3,23 @@ import "./App.css";
 const App = () => {
   const [todos, setTodos] = useState([]);
   
-  // Add the handlesubmit code here
+  function handleSubmit(e){
+    e.preventDefault();
+    let  todo = document.getElementById('todoAdd').value; 
+    const newTodo = {
+      id: new Date().getTime(),
+      text: todo.trim(),
+      completed: false
+    };
+
+    if(newTodo.text.length> 0){
+      setTodos([...todos].concat(newTodo));
+    }else{
+      alert("Enter a vlaid task");
+    }
+    document.getElementById('todoAdd').value="";
+
+  }
   
   
   // Add the deleteToDo code here
@@ -16,12 +32,19 @@ const App = () => {
 
   
 return(
-<div className ="App">
-<h1>Todo List</h1>
-<form>
-<input type ="text" align ="right" id= 'todoAdd'/>
-<button type ="submit">Add Todo</button>
-</form>
+<div id="todo-list">
+  <h1>Todo List</h1>
+  <form onSubmit={handleSubmit}>
+    <input type="text" id ="todoAdd" />
+    <button type="submit">Add todo</button>
+  </form>
+  {todos.map((todo)=>
+    <div className="todo" key={todo.id}>
+      <div className="todo-text">{todo.text}</div>
+    </div>
+
+  )}
+
 </div>
 );
 };
